3398a39b6b7c99cf183fdd18da76ece12043a0ad
Ic3Sandy <monkey_king2@hotmail.com>
2022-09-01 15:36:48 +0700

    🚧 Update for generate raw tx create contract

--- createRawTx.ts
+++ createRawTx.ts
@@ -3,32 +3,52 @@
  */
 import { RPCClient } from "firovm-sdk";
 import { promises as fs } from "fs";
-const dirTx = "tx";
+import { byteCode } from "./erc20";
 
-async function createTransaction(
-  rpc: RPCClient,
-  utxo: any,
-  gas: number,
-  addr: string
-) {
-  const left = (utxo.amount - gas).toFixed(8);
-  const data: any = [
-    [{ txid: utxo.txid, vout: utxo.vout }],
-    [{ [addr]: left }],
-  ];
+const dirTx = "tx";
+const url = "http://test:test1234@127.0.0.1:3889"; // rpc url
+const numUTXO = 10; // number of UTXO to prepare
+const fee = 0.01; // fee per transaction
+const addr = "qUe9cwiX81Y729BMgPMV4enHVBbDPDj7Xf"; // Address from getnewaddress
+const createContract = true; // create contract or send payment
+const gasLimit = 2500000; // gas limit for create contract
+const gasPrice = "0.0000004"; // gas price for create contract
+const rpc = new RPCClient(url);
+
+async function createTransaction(utxo: any) {
+  const left = (utxo.amount - fee).toFixed(8);
+  let data: any[];
+
+  if (createContract) {
+    data = [
+      [{ txid: utxo.txid, vout: utxo.vout }],
+      [
+        {
+          contract: {
+            bytecode: byteCode,
+            gasLimit,
+            gasPrice,
+            senderAddress: addr,
+          },
+        },
+      ],
+    ];
+  } else {
+    data = [[{ txid: utxo.txid, vout: utxo.vout }], [{ [addr]: left }]];
+  }
 
   const { result, error } = await rpc.rpc("createrawtransaction", data);
   if (error) {
-    console.log(error);
+    console.log("createrawtransaction:", error);
     throw error;
   }
   return result;
 }
 
-async function getUtxoList(rpc: RPCClient) {
+async function getUtxoList() {
   const { result, error } = await rpc.rpc("listunspent");
   if (error) {
-    console.log(error);
+    console.log("listunspent:", error);
     throw error;
   }
   const utxoList = result.filter((utxo: any) => utxo.amount >= 1);
@@ -39,29 +59,35 @@ async function getUtxoList(rpc: RPCClient) {
 /**
  * prepare raw Tx
  */
-async function createRaxTx(
-  rpc: RPCClient,
-  num: number,
-  gas: number,
-  addr: string
-) {
-  console.log("start create rawTx");
+async function createRaxTx() {
+  console.log("Start create raw transactions");
 
-  const utxoList = await getUtxoList(rpc);
-  if (utxoList.length < num) {
-    console.log("not enough UTXOs");
+  const utxoList = await getUtxoList();
+  if (utxoList.length < numUTXO) {
+    console.log("Not enough UTXOs");
     return;
   }
 
   await fs.writeFile("sendrawtransaction.http", "");
-  for (let i = 1; i <= num; i++) {
-    const rawTransaction = await createTransaction(rpc, utxoList[i], gas, addr);
-    const { result, error } = await rpc.rpc("signrawtransactionwithwallet", [
-      rawTransaction,
-    ]);
-    if (error) {
-      console.log(error);
-      throw error;
+  for (let i = 1; i <= numUTXO; i++) {
+    let rawTransaction = await createTransaction(utxoList[i]);
+    let res;
+
+    if (createContract) {
+      res = await rpc.rpc("signrawsendertransactionwithwallet", [
+        rawTransaction,
+      ]);
+      if (res.error) {
+        console.log(`signrawsendertransactionwithwallet:`, res.error);
+        throw res.error;
+      }
+      rawTransaction = res.result.hex;
+    }
+
+    res = await rpc.rpc("signrawtransactionwithwallet", [rawTransaction]);
+    if (res.error) {
+      console.log(`signrawtransactionwithwallet:`, res.error);
+      throw res.error;
     }
 
     await fs.writeFile(
@@ -69,7 +95,7 @@ async function createRaxTx(
       JSON.stringify({
         jsonrpc: "2.0",
         method: "sendrawtransaction",
-        params: [result.hex],
+        params: [res.result.hex],
         id: 1,
       })
     );
@@ -79,27 +105,14 @@ async function createRaxTx(
     );
     console.log(`tx${i}`);
   }
-
   console.log("finish");
 }
 
-/**
- * rpc url
- * number of UTXO to prepare
- * gas per transaction
- */
 (async () => {
-  const url = "http://test:test1234@127.0.0.1:3889";
-  const num = 10;
-  const gas = 0.01;
-  const addr = "qUe9cwiX81Y729BMgPMV4enHVBbDPDj7Xf";
-  const rpc = new RPCClient(url);
-
   try {
     await fs.access(dirTx);
   } catch (error) {
     fs.mkdir(dirTx);
   }
-
-  await createRaxTx(rpc, num, gas, addr);
+  await createRaxTx();
 })();
